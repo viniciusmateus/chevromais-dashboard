e5d714f5f317044ff314837808a2ec78a50d51c0
Vinicius <o.x@live.de>
2026-08-20 15:12:54 -0300

    Chg: adiciona adaptador @astrojs/node para o Firebase App Hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import node from '@astrojs/node';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
+  integrations: [react(), tailwind()],
 });
